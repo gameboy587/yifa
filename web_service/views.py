0ea8bcb8b3963d52d8ff99c0f35f4f138e60904c
Botu Sun <gameboy1024@hotmail.com>
2015-05-12 23:42:07 +0200

    Added the web_service app

--- web_service/views.py
+++ web_service/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def confirm_reservation(request):
+	# Obtain request body and user information
+	
+	# Make a DB request to get the token
+	# Call Xinge API to push notification to client side
+	# Return Success message
+    return HttpResponse("Hello, world. You're at the web_service index.")
